6f501391805072fe0b124277da4a44ff48e2fd37
Leo Developer <leo@git.leodev.win>
2025-03-08 20:50:42 +0100

    fix #162

--- packages/webcrack/src/deobfuscate/control-flow-switch.ts
+++ packages/webcrack/src/deobfuscate/control-flow-switch.ts
@@ -21,10 +21,7 @@ export default {
       m.arrayOf(
         m.switchCase(
           m.stringLiteral(m.matcher((s) => /^\d+$/.test(s))),
-          m.anyList(
-            m.zeroOrMore(),
-            m.or(m.continueStatement(), m.returnStatement()),
-          ),
+          m.zeroOrMore(),
         ),
       ),
     );
